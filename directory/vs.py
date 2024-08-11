19f8d5c4bbe722c989d3e473250ca8badb48f2c4
Behnoush <behnoush.shahraeini@gmail.com>
2024-08-11 15:33:30 +0330

    change

--- directory/vs.py
+++ directory/vs.py
@@ -1,10 +1,10 @@
 import os , time , datetime
 def create_directory(directory_name):
     os.mkdir(directory_name) # ساخت دایرکتوری 
-    zaman_feli = time.time() #زمات فعلی 
+    zaman_feli = time.time() #زمان فعلی 
     creation_datetime=datetime.datetime.fromtimestamp(zaman_feli) # ساخت دیت تایم 
-    current_datetime = datetime.datetime.now() # زملت فعلی برای مدت زمان سپری شده
-    time_differance = current_datetime - creation_datetime # زمات سپری شده 
+    current_datetime = datetime.datetime.now() # زمان فعلی برای مدت زمان سپری شده
+    time_differance = current_datetime - creation_datetime # زمان سپری شده 
     
     print(f"Directory name ---> {directory_name}, and creation time is :{creation_datetime}, time since creation:{time_differance}")
     
